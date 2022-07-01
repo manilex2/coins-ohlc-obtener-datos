616941e78f3359fb3551b3d97a1826d5e41c0b41
Manuel Pereira <manilex2@gmail.com>
2022-07-01 11:28:23 -0500

    actualizadas funciones

--- AWS/index.js
+++ AWS/index.js
@@ -13,32 +13,52 @@ exports.handler = async function (event) {
             database: database.database
         });
         var sql = `SELECT name FROM ${process.env.TABLE_COINS_LIST}`;
+        async function delay(ms) {
+            return await new Promise(resolve => setTimeout(resolve, ms));
+          }
         conexion.query(sql, async function (err, resultado) {
             if (err) throw err;
-            await guardarOHLC(resultado, process.env.PERIODO, process.env.TABLE_OHLC);
-            await finalizarEjecucion();
+            var grupo = Math.floor(resultado.length/process.env.NUM_GRUPOS);
+            var contador = grupo;
+            var cantGrupos = [contador];
+            while (contador < resultado.length) {
+                contador = contador + grupo;
+                cantGrupos.push(contador);
+            }
+            if (contador > resultado.length) {
+                cantGrupos.pop();
+                cantGrupos.push(resultado.length);
+            }
+            var inicio = 0;
+            for (let i = 0; i < cantGrupos.length; i++) {
+                const fin = cantGrupos[i];
+                console.log(`Entre ${i+1} veces`);
+                await delay(process.env.DELAY);
+                guardarOHLC(resultado, inicio, fin);
+                inicio = fin;
+            }
         });
-        async function guardarOHLC(resultado, tiempo, variable){
-            for (let i = 0; i < resultado.length; i++) {
+        async function guardarOHLC(resultado, inicio, fin){
+            for (let i = inicio; i < fin; i++) {
                 var coin = resultado[i].name;
-                await fetch(`https://api.coingecko.com/api/v3/coins/${coin}/ohlc?vs_currency=usd&days=${tiempo}`)
+                await fetch(`https://api.coingecko.com/api/v3/coins/${coin}/ohlc?vs_currency=usd&days=${process.env.PERIODO}`)
                 .then((res) => {
                     return res.json();
                 }).then((json) => {
                     var ohlc = json;
-                    guardarBaseDeDatos(coin, ohlc, variable);
+                    guardarBaseDeDatos(coin, ohlc);
                 }).catch((err) => {
                     console.error(err);
                 });
             }
         };
-        async function guardarBaseDeDatos(coin, datos, tabla){
+        async function guardarBaseDeDatos(coin, datos){
             for (let i = 0; i < datos.length; i++) {
                 datos[i][0] = new Date(datos[i][0]);
                 let month = datos[i][0].getMonth();
                 let monthBD = month + 1;
                 let fechaBD = datos[i][0].getFullYear() + '-' + monthBD + '-' + datos[i][0].getDate() + ' ' + datos[i][0].getHours() + ':' + datos[i][0].getMinutes() + ':' + datos[i][0].getSeconds();
-                var sql = `INSERT INTO ${tabla} (
+                var sql = `INSERT INTO ${process.env.TABLE_OHLC} (
                     name,
                     fecha, 
                     open,
@@ -55,16 +75,13 @@ exports.handler = async function (event) {
                         ${datos[i][4]} AS close
                     ) AS tmp
                     WHERE NOT EXISTS (
-                        SELECT fecha, name FROM ${tabla} WHERE fecha = '${fechaBD}' AND name = '${coin}'
+                        SELECT fecha, name FROM ${process.env.TABLE_OHLC} WHERE fecha = '${fechaBD}' AND name = '${coin}'
                     ) LIMIT 1`;
                 conexion.query(sql, function (err, resultado) {
                     if (err) throw err;
                 });
             }
         };
-        async function finalizarEjecucion() {
-            conexion.end();
-        }
     });
     return promise;
 };
